55cfd85cf5d03e5fbd1a85b9a9b83bc611f7ad2d
EladMorr <eladmor95@gmail.com>
2021-12-26 20:25:24 +0200

    PersonalFragment dosn't work..

--- app/src/main/java/com/example/firebase/ui/fragment/PersonalFragment.java
+++ app/src/main/java/com/example/firebase/ui/fragment/PersonalFragment.java
@@ -1,14 +1,25 @@
 package com.example.firebase.ui.fragment;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.firebase.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +33,16 @@ public class PersonalFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    TextView timerText;
+    Button stopStartButton;
+    Timer timer;
+    TimerTask timerTask;
+    Double time;
+    boolean timerStarted;
 
     public PersonalFragment() {
         // Required empty public constructor
@@ -42,6 +60,7 @@ public class PersonalFragment extends Fragment {
     public static PersonalFragment newInstance(String param1, String param2) {
         PersonalFragment fragment = new PersonalFragment();
         Bundle args = new Bundle();
+
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
@@ -51,16 +70,98 @@ public class PersonalFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView timerText;
+        Button stopStartButton;
+        Timer timer;
+        TimerTask timerTask;
+        Double time = 0.0;
+        boolean timerStarted = false;
+
+        timerText = getView().findViewById(R.id.timerText);
+        stopStartButton = getView().findViewById(R.id.startStopBTN);
+
+        timer = new Timer();
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_page, container, false);
+    public void resetTapped(View view) {
+        AlertDialog.Builder resetAlert = new AlertDialog.Builder(getActivity());
+        resetAlert.setTitle("Reset Timer");
+        resetAlert.setMessage("Are you sure you want to reset the timer?");
+        resetAlert.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (timerTask != null) {
+                    timerTask.cancel();
+                    setButtonUI("START", R.color.green);
+                    time = 0.0;
+                    timerStarted = false;
+                    timerText.setText(formatTime(0, 0, 0));
+                }
+            }
+        });
+        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //do nothing
+            }
+        });
+        resetAlert.show();
+    }
+
+    public void startStopTapped(View view) {
+        if (timerStarted == false) {
+            timerStarted = true;
+            setButtonUI("STOP", R.color.red);
+
+            startTimer();
+        } else {
+            timerStarted = false;
+            setButtonUI("START", R.color.green);
+
+            timerTask.cancel();
+        }
+    }
+
+    private void setButtonUI(String start, int color) {
+        stopStartButton.setText(start);
+        stopStartButton.setTextColor(ContextCompat.getColor(getActivity(), color));
+    }
+
+    private void startTimer() {
+        timerTask = new TimerTask() {
+            private Handler mHandler = new Handler(Looper.getMainLooper());
+
+            @Override
+            public void run() {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        time++;
+                        timerText.setText(getTimerText());
+                    }
+                });
+            }
+
+        };
+        timer.scheduleAtFixedRate(timerTask, 0, 1000);
+    }
+
+    private String getTimerText() {
+        int rounded = (int) Math.round(time);
+
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours) {
+        return String.format("%02d", hours) + " : " + String.format("%02d", minutes) + " : " + String.format("%02d", seconds);
     }
 }
