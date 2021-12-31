35b334f2f9f4cc55482b809ef1461746c7fefaf1
EladMorr <eladmor95@gmail.com>
2021-12-31 07:28:24 +0200

    PersonalFragment need to countUp

--- app/src/main/java/com/example/firebase/ui/fragment/PersonalFragment.java
+++ app/src/main/java/com/example/firebase/ui/fragment/PersonalFragment.java
@@ -1,167 +1,47 @@
 package com.example.firebase.ui.fragment;
 
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.firebase.R;
 
 import java.util.Timer;
-import java.util.TimerTask;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PersonalFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PersonalFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    TextView timerText;
-    Button stopStartButton;
-    Timer timer;
-    TimerTask timerTask;
-    Double time;
-    boolean timerStarted;
-
+    ImageButton circleClockBTN;
+    boolean timerStarted = false;
+    TextView mClock;
     public PersonalFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment personalPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PersonalFragment newInstance(String param1, String param2) {
-        PersonalFragment fragment = new PersonalFragment();
-        Bundle args = new Bundle();
-
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView timerText;
-        Button stopStartButton;
-        Timer timer;
-        TimerTask timerTask;
-        Double time = 0.0;
-        boolean timerStarted = false;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
-        timerText = getView().findViewById(R.id.timerText);
-        stopStartButton = getView().findViewById(R.id.startStopBTN);
+        View v = inflater.inflate(R.layout.fragment_personal_page, container, false);
 
-        timer = new Timer();
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    public void resetTapped(View view) {
-        AlertDialog.Builder resetAlert = new AlertDialog.Builder(getActivity());
-        resetAlert.setTitle("Reset Timer");
-        resetAlert.setMessage("Are you sure you want to reset the timer?");
-        resetAlert.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+        circleClockBTN = v.findViewById(R.id.circleBTN);
+        mClock = v.findViewById(R.id.timerTXT);
+        circleClockBTN.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (timerTask != null) {
-                    timerTask.cancel();
-                    setButtonUI("START", R.color.green);
-                    time = 0.0;
-                    timerStarted = false;
-                    timerText.setText(formatTime(0, 0, 0));
-                }
+            public void onClick(View view) {
+
+                mClock.setText("00:00:00");
             }
         });
-        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //do nothing
-            }
-        });
-        resetAlert.show();
+
+        return v;
     }
 
-    public void startStopTapped(View view) {
-        if (timerStarted == false) {
-            timerStarted = true;
-            setButtonUI("STOP", R.color.red);
-
-            startTimer();
-        } else {
-            timerStarted = false;
-            setButtonUI("START", R.color.green);
-
-            timerTask.cancel();
-        }
-    }
-
-    private void setButtonUI(String start, int color) {
-        stopStartButton.setText(start);
-        stopStartButton.setTextColor(ContextCompat.getColor(getActivity(), color));
-    }
-
-    private void startTimer() {
-        timerTask = new TimerTask() {
-            private Handler mHandler = new Handler(Looper.getMainLooper());
-
-            @Override
-            public void run() {
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        time++;
-                        timerText.setText(getTimerText());
-                    }
-                });
-            }
-
-        };
-        timer.scheduleAtFixedRate(timerTask, 0, 1000);
-    }
-
-    private String getTimerText() {
-        int rounded = (int) Math.round(time);
-
-        int seconds = ((rounded % 86400) % 3600) % 60;
-        int minutes = ((rounded % 86400) % 3600) / 60;
-        int hours = ((rounded % 86400) / 3600);
-
-        return formatTime(seconds, minutes, hours);
-    }
-
-    private String formatTime(int seconds, int minutes, int hours) {
-        return String.format("%02d", hours) + " : " + String.format("%02d", minutes) + " : " + String.format("%02d", seconds);
-    }
 }
